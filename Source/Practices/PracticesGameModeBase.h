cdb2935a0464557a7748e79b967d1040a7d00257
zYr <zyr_zyr_@sohu.com>
2017-04-03 17:44:26 +0800

    Practices Init With Starter Content

---
+++ Source/Practices/PracticesGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "PracticesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PRACTICES_API APracticesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
